ca8687861c67fe39c29805c884f1390f61e29163
JevDev2304 <jvaldes245@soyudemedellin.edu.co>
2024-05-06 09:17:53 -0500

    I make CORS

--- main.py
+++ main.py
@@ -10,9 +10,22 @@ from routers.access import accessRouter
 
 from routers.uploadFiles import filesRouter
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
+
+
+
+
 
 app = FastAPI()
 
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow connections from any origin
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(ownerRouter)
 app.include_router(agentRouter)
