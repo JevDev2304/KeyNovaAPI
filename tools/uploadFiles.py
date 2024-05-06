e7fbb9c511eaf957f22cf656b21cf184cdc3d20a
JevDev2304 <jvaldes245@soyudemedellin.edu.co>
2024-05-06 10:31:25 -0500

    Solving bug upload Video

--- tools/uploadFiles.py
+++ tools/uploadFiles.py
@@ -4,16 +4,16 @@ from fastapi import HTTPException
 
 async def upload_video(VIDEODIR , file :UploadFile = File(...), ):
     if file.filename.endswith(".mp4"):
-        file.filename = f"{uuid.uuid4()}.mp4"
+        file.filename = f"{uuid.uuid4()}.jpg"
         contents = await file.read()
-
-    # save the file
-        with open(f"{VIDEODIR}{file.filename}", "wb") as f:
-            f.write(contents)
-        return {"filename": file.filename}
     else:
         raise HTTPException(status_code=400,
-                            detail="invalid filetype . Please, upload an video filetype (.mp4).")
+                            detail="invalid filetype . Please, upload a video filetype (.mp4)")
+
+        # save the file
+    with open(f"{VIDEODIR}{file.filename}", "wb") as f:
+        f.write(contents)
+    return {"filename": file.filename}
 
 async def upload_img(IMGDIR , file :UploadFile = File(...), ):
     if file.filename.endswith(".jpg") or file.filename.endswith(".png") or file.filename.endswith(".jpeg"):
