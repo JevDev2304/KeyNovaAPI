ee51d306712ed0b723c0e46a9be9a47c1230368e
JevDev2304 <jvaldes245@soyudemedellin.edu.co>
2024-05-06 10:15:44 -0500

    Tested, video and image upload access with endpoint

--- routers/uploadFiles.py
+++ routers/uploadFiles.py
@@ -1,17 +1,24 @@
 from fastapi import APIRouter, status, HTTPException,File, UploadFile
 from tools.uploadFiles import upload_video, upload_img
+import os
 
 filesRouter = APIRouter(prefix="/uploadFiles", tags=["uploadFiles"])
-IMAGEDIR= "/home/jevdev/PycharmProjects/Project1/static/images/"
-VIDEODIR = "/home/jevdev/PycharmProjects/Project1/static/videos"
+IMAGEDIR = "../static/images/"
+VIDEODIR = "../static/videos/"
+current_dir = os.path.dirname(os.path.realpath(__file__))
+
+# Combina las rutas relativas con el directorio actual para obtener las rutas absolutas
+absolute_imagedir = os.path.join(current_dir, IMAGEDIR)
+absolute_videodir = os.path.join(current_dir, VIDEODIR)
+
 @filesRouter.post("/img")
 async def create_upload_img(file: UploadFile = File(...)):
-    result = await upload_img(IMAGEDIR,file)
+    result = await upload_img(absolute_imagedir,file)
     return result
 
 
 @filesRouter.post("/video")
 async def create_upload_video(file: UploadFile = File(...)):
-    result = await upload_video(VIDEODIR,file)
+    result = await upload_video(absolute_videodir,file)
     return result
 
