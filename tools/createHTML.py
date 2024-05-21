9db9b6caa88759e1ff8c2ee1c627f95181aee070
Hidalgo87 <juanfgomez6@gmail.com>
2024-05-21 01:57:23 -0500

    html listos

--- tools/createHTML.py
+++ tools/createHTML.py
@@ -1,10 +1,147 @@
-#TODO JUANFER inventoryHTML(inventory)
 def OTPHTML(num: int) -> str:
-    return f"<body> OTP :{num} </body>"
+    ht = """
+    <!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>OTP - Nova Gesti贸n de Inventarios</title>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap" rel="stylesheet">
+    <style>
+        body {
+            font-family: 'Poppins', sans-serif;
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            background-color: #f0f0f0;
+        }
+        .container {
+            width: 40%;
+            max-width: 600px;
+            padding: 20px;
+            background-color: white;
+            border-radius: 16px;
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+            text-align: center;
+        }
+        .header {
+            margin-bottom: 40px;
+        }
+        .header h1 {
+            color: #F2542D;
+            margin: 0;
+        }
+        .otp-section {
+            background-color: #fff6ed;
+            border: 2px solid #F2542D;
+            border-radius: 16px;
+            padding: 20px;
+            margin: 20px 0;
+            color:black;
+            font-weight: bold;
+            font-size: 24px;
+        }
+        .warning {
+            color: #F2542D;
+            margin: 20px 0;
+            font-weight: bold;
+        }
+        .footer {
+            margin-top: 40px;
+        }
+        .footer p {
+            color: #000;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <div class="header">
+            <h1> Verificación de dos pasos </h1>
+        </div>
+        <div class="otp-section">
+            Tu código OTP es: <span>"""+str(num)+"""</span>
+        </div>
+        <div class="warning">
+            No compartas este código con nadie.
+        </div>
+        <div class="footer">
+            <p>Gracias por confiar en Nova.</p>
+        </div>
+    </div>
+</body>
+</html>"""
+    return ht
 
 
 def maintenanceHTML(maintenance: dict) -> str:
-    return f"<body>{str(maintenance)}</body>"
+    ht = '''
+    <!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Detalles de Mantenimiento</title>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap" rel="stylesheet">
+    <style>
+        body {
+            font-family: 'Poppins', sans-serif;
+            margin: 0;
+            padding: 20px;
+            background-color: #fff6ed;
+            color: #0E9594;
+        }
+        .container {
+            max-width: 600px;
+            margin: 0 auto;
+            background: #ffffff;
+            border: 1px solid #F2542D;
+            border-radius: 10px;
+            padding: 20px;
+        }
+        .header {
+            text-align: center;
+            margin-bottom: 20px;
+        }
+        .header h1 {
+            margin: 0;
+            color: #000;
+        }
+        .content {
+            margin-bottom: 20px;
+        }
+        .content p {
+            margin: 10px 0;
+        }
+        .footer {
+            text-align: center;
+            color: #562C2C;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <div class="header">
+            <h1>Detalles de Mantenimiento</h1>
+        </div>
+        <div class="content">
+            <p>Este correo se envió para notificar que se ha realizado un mantenimiento en su vivienda. 
+            A continuación, se presentan los detalles:</p>
+            <p><strong>Descripción: </strong>'''+maintenance["descripcion"]+'''</p>
+            <p><strong>Fecha: </strong>'''+maintenance["fecha"]+'''</p>
+        </div>
+        <div class="footer">
+            <p>Gracias por su atención.</p>
+        </div>
+    </div>
+</body>
+</html>
+'''
+    return ht
 
 
 def inventoryHTML(inventory: dict) -> str:
