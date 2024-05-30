9360f7370348dfd7bebb9fb9bb56fad670115cd2
Hidalgo87 <juanfgomez6@gmail.com>
2024-05-30 11:53:32 -0500

    Corregir Property

--- schemas/property.py
+++ schemas/property.py
@@ -2,8 +2,9 @@ def property_schema(property) -> dict:
     return {"idPropiedad" : int(property[0]),
             "Propietario_idPropietario" : int(property[1]),
             "direccion" : str(property[2]),
-            "imagen" : str(property[3])
-           }
+            "imagen" : str(property[3]),
+            "firmado": int(property[4])}
+
 def properties_schema(properties) -> list:
     return [property_schema(property) for property in properties]
 
